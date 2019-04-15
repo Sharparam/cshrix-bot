509bd913da845d30e69df3d5130516d013680be5
Adam Hellberg <sharparam@sharparam.com>
2019-04-15 03:15:17 +0200

    Move data class to correct namespace

--- src/Cshrix/Data/RoomIdContainer.cs
+++ src/Cshrix/Data/RoomIdContainer.cs
@@ -6,10 +6,8 @@
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
 
-namespace Cshrix
+namespace Cshrix.Data
 {
-    using Data;
-
     using Newtonsoft.Json;
 
     public readonly struct RoomIdContainer
