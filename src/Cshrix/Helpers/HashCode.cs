edb59ca1ec521e2a1bf473a1805de39c8bc2454f
Adam Hellberg <sharparam@sharparam.com>
2019-04-15 03:14:07 +0200

    Code cleanup

--- src/Cshrix/Helpers/HashCode.cs
+++ src/Cshrix/Helpers/HashCode.cs
@@ -71,7 +71,13 @@ namespace Cshrix.Helpers
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Combine<T1, T2, T3, T4, T5, T6>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6)
+        public static int Combine<T1, T2, T3, T4, T5, T6>(
+            T1 value1,
+            T2 value2,
+            T3 value3,
+            T4 value4,
+            T5 value5,
+            T6 value6)
         {
             var hash = Initializer;
             hash = hash * Multiplier + (value1?.GetHashCode() ?? 0);
@@ -84,7 +90,14 @@ namespace Cshrix.Helpers
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Combine<T1, T2, T3, T4, T5, T6, T7>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7)
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7>(
+            T1 value1,
+            T2 value2,
+            T3 value3,
+            T4 value4,
+            T5 value5,
+            T6 value6,
+            T7 value7)
         {
             var hash = Initializer;
             hash = hash * Multiplier + (value1?.GetHashCode() ?? 0);
@@ -98,7 +111,15 @@ namespace Cshrix.Helpers
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8>(T1 value1, T2 value2, T3 value3, T4 value4, T5 value5, T6 value6, T7 value7, T8 value8)
+        public static int Combine<T1, T2, T3, T4, T5, T6, T7, T8>(
+            T1 value1,
+            T2 value2,
+            T3 value3,
+            T4 value4,
+            T5 value5,
+            T6 value6,
+            T7 value7,
+            T8 value8)
         {
             var hash = Initializer;
             hash = hash * Multiplier + (value1?.GetHashCode() ?? 0);
