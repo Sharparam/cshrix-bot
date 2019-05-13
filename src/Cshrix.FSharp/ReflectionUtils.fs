bff8b76ded672c2ae43292346f0edca74bec7ca1
Adam Hellberg <adam.hellberg@ninetech.com>
2019-05-13 15:32:28 +0200

    Move reflection utils to F#

--- src/Cshrix.FSharp/ReflectionUtils.fs
+++ src/Cshrix.FSharp/ReflectionUtils.fs
@@ -4,9 +4,22 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/// Contains helper methods for performing reflection tasks.
 module Cshrix.Utilities.ReflectionUtils
 open System
 
+/// <summary>
+/// Checks if a type is not a value type and nullable.
+/// </summary>
+/// <param name="type">The type to check.</param>
+/// <returns><c>true</c> if the type is nullable, otherwise <c>false</c>.</returns>
+[<CompiledName("IsNullableType")>]
 let isNullableType (t:Type) = t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<Nullable<_>>
 
+/// <summary>
+/// Checks if a type (including value types) is nullable.
+/// </summary>
+/// <param name="type">The type to check.</param>
+/// <returns><c>true</c> if the type is nullable, otherwise <c>false</c>.</returns>
+[<CompiledName("IsNullable")>]
 let isNullable (t:Type) = not t.IsValueType || t |> isNullableType
